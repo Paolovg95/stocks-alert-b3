6103dca1cf094e19d950ca375f30798458345e4d
Paolovg95 <paolo9517@gmail.com>
2022-06-09 22:43:50 -0400

    SMTP Email modified

--- scraping/cron.py
+++ scraping/cron.py
@@ -1,8 +1,23 @@
 from .models import Stock
+import smtplib
+from email.message import EmailMessage
+
 
 def my_scheduled_job():
-  name = "3R Petroleum ON"
-  stock = Stock.objects.get(title=name)
-  stock.price = 96
-  stock.save()
-  print(stock.price)
+
+
+    # url = "https://stocks-price-alert-b3.herokuapp.com/"
+    # email = EmailMessage()
+    # email['from'] = 'Python <vargasdegasperi@orastudio.tech>'
+    # email['to'] = 'paolo9517@gmail.com'
+    # email['subject'] = 'Stock Alert'
+
+    # email.set_content('New Opportunity {}'.format(url))
+
+    # with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+    #     smtp.ehlo()
+    #     smtp.starttls()
+    #     smtp.ehlo()
+
+    #     smtp.login('vargasdegasperi@orastudio.tech', 'password')
+    #     smtp.send_message(email)
