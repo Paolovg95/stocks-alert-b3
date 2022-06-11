ee4340d4ad7d90f883ac04bc54bfc96a77d57536
Paolovg95 <paolo9517@gmail.com>
2022-06-11 01:37:05 -0400

    Take environment variables from .env file

--- stocks/settings.py
+++ stocks/settings.py
@@ -19,6 +19,7 @@ environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
