3fcb1f37c877c4dd3884919fa8b29121c615eb1f
Paolovg95 <paolo9517@gmail.com>
2022-06-21 14:25:01 -0400

    Environ updated

--- stocks/urls.py
+++ stocks/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from pages.views import home_view
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view, name="home"),
